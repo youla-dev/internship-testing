68d564cac22dcc27ae19d05eca7ba7fcefbaa5c4
Tanya Chuk <33889402+tanya-chuk@users.noreply.github.com>
2022-02-24 17:13:26 +0300

    Create checkDate.js

---
+++ Task 1/checkDate.js
@@ -0,0 +1,28 @@
+function checkDate(timestamp) {
+    var day = new Date(timestamp).getDate();
+    var month = new Date(timestamp).getMonth();
+    var year = new Date(timestamp).getFullYear();
+    var hour = new Date(timestamp).getHours();
+
+    const current_Date = new Date(Date.now());
+    const current_day = current_Date.getDate();
+    const current_month = current_Date.getMonth();
+    const currentYear = current_Date.getFullYear();
+
+    let isSameDate = false;
+
+    if (year == currentYear) {
+        if (month == current_month) {
+            if (day == current_day) {
+                isSameDate = true;
+            } else {
+                isSameDate = false;
+            }
+        }
+    }
+
+    return {
+        isSameDate: isSameDate,
+        dayPeriod: hour > 11 ? 'pm' : 'am'
+    }
+}
