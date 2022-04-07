8c92ff5e35a7b740ecfd8480d99b6ace9a463155
kseniaspasoevich <kseniya.spasoyevich@gmail.com>
2022-04-07 21:53:16 +0300

    add: task 1

--- Task 1/checkDate.js
+++ Task 1/checkDate.js
@@ -1,23 +1,24 @@
 function checkDate(timestamp) {
-    var day = new Date(timestamp * 1000).getDate();
-    var month = new Date(timestamp * 1000).getMonth();
-    var year = new Date(timestamp * 1000).getFullYear();
-    var hour = new Date(timestamp * 1000).getHours();
+    //Для наглядности, все названия переменных будут написаны в стиле camelCase
 
-    const current_Date = new Date(Date.now());
-    const current_day = current_Date.getDate();
-    const current_month = current_Date.getMonth() + 1;
-    const currentYear = current_Date.getFullYear();
+    //Const, потому что значения переменных ниже в функции не будут меняться, зависят только от параметра timestamp
+    const  day = new Date(timestamp * 1000).getDate();
+    const  month = new Date(timestamp * 1000).getMonth() + 1; //+1, т.к. без него присваивается предыдущий месяц
+    const  year = new Date(timestamp * 1000).getFullYear();
+    const  hour = new Date(timestamp * 1000).getHours();
+
+    const currentDate = new Date(Date.now());
+    const currentDay = currentDate.getDate();
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentYear = currentDate.getFullYear();
 
     let isSameDate = false;
 
-    if (year == currentYear) {
-        if (month == current_month) {
-            if (day == current_day) {
-                isSameDate = true;
-            } else {
-                isSameDate = false;
-            }
+    //для более точного сравнения, используeтся строгое равенство
+    //избавляемся от изначального присваивания false, т.к. переменной оно уже присвоено выше
+    if (year === currentYear) {
+        if (month === currentMonth) {
+            isSameDate = day === currentDay;
         }
     }
 
@@ -26,3 +27,6 @@ function checkDate(timestamp) {
         dayPeriod: hour > 11 ? 'pm' : 'am'
     }
 }
+
+console.log(checkDate(2345789)); //Wed, 28 Jan 1970 03:36:29 GMT, { isSameDate: false, dayPeriod: 'am' }
+console.log(checkDate(1649359348)); //Thu, 07 Apr 2022 19:22:28 GMT, { isSameDate: true, dayPeriod: 'pm' }
